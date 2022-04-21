3e4bfd1be116775db4f709f11716db4242bb7626
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-21 22:44:52 +0100

    components setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route path="/" component={Home} />
+            <Route path="/movie/:imdbID" component={MovieDetail} />
+            <Route component={PageNotFound} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
