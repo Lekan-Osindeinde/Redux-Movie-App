11f0137ec6ca4dad7fea826223ed917d32da8252
Lekan Osindeinde <osindeindelekan@gmail.com>
2022-04-23 03:28:15 +0100

    movieSlice, reducers store

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import MovieApi from "../../common/apis/movieApi";
+import { APIKey } from "../../common/apis/MovieApiKey";
+import { addMovies } from "../../features/movies/movieSlice";
 import MovieListing from "../MovieListing/MovieListing";
 
 const Home = () => {
+  const movieText = "Harry";
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const response = await MovieApi.get(
+        `?apiKey=${APIKey}&s=${movieText}&type=movie`
+      ).catch((err) => {
+        console.log("Err :", err);
+      });
+      dispatch(addMovies(response.data));
+    };
+    fetchMovies();
+  }, []);
   return (
     <div>
       <div className="banner-img"></div>
